08a162e2668e25ad1638cb2511ddf57975d24099
Venky <venkatash.sv@gmail.com>
2024-08-29 12:55:49 +0530

    init

---
+++ src/index.ts
@@ -0,0 +1,37 @@
+import express, { Application, Request, Response } from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+// import connectDB from "./config/db";
+import { notFound, errorHandler } from "./middlewares/ErrorMiddleware";
+import AuthRoutes from "./routes/AuthRoutes";
+
+const app: Application = express();
+
+dotenv.config();
+
+// connectDB();
+
+app.use(express.json());
+
+// Enable CORS for all routes
+app.use(
+  cors({
+    origin: "*",
+  })
+);
+
+// Default
+app.get("/api", (req: Request, res: Response) => {
+  res.status(201).json({ message: "Welcome to Absinthe Backend" });
+});
+
+// User Route
+// app.use("/api/auth", AuthRoutes);
+
+// Middleware
+app.use(notFound);
+app.use(errorHandler);
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, (): void => console.log(`Server is running on ${PORT}`));
