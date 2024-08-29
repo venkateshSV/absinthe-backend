b8a839377c0154715d525f65da51039a4da5345a
Venky <venkatash.sv@gmail.com>
2024-08-29 12:56:40 +0530

    init2

---
+++ dist/routes/AuthRoutes.js
@@ -0,0 +1,12 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+// import { login, register, getAll } from "../controllers/AuthController";
+const router = express_1.default.Router();
+// router.route("/").get(getAll).post(login);
+// router.post("/register", register);
+exports.default = router;
+//# sourceMappingURL=AuthRoutes.js.map
---
+++ dist/utils/generateToken.js
@@ -0,0 +1,9 @@
+// import jwt from "jsonwebtoken";
+// const generateToken = (id: string) => {
+//   const token = jwt.sign({ id }, process.env.JWT_SECRET as string, {
+//     expiresIn: "30d",
+//   });
+//   return token;
+// };
+// export default generateToken;
+//# sourceMappingURL=generateToken.js.map
